90044f2b1880ac0d74175ac28e42763010dbbccc
Gregory Goodreau <goodreauh@gmail.com>
2020-06-14 00:11:46 -0500

    testing

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+from ..tools.user_admin import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
+
+@app.route('/admin')
+def admin_render():
+    return render_template('admin.html')
