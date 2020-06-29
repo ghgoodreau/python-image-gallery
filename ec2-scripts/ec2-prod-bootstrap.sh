6c89a3186a8c4f3e003a2e41be82fb18d0768df1
Gregory Goodreau <goodreauh@gmail.com>
2020-06-28 19:31:38 -0500

    corrected typo

--- ec2-scripts/ec2-prod-bootstrap.sh
+++ ec2-scripts/ec2-prod-bootstrap.sh
@@ -3,5 +3,4 @@
 
 export IMAGE_GALLERY_BOOTSTRAP_VERSION="1.0"
 
-aws s3 cp s3://ghg.rocks.image-gallery/ec2-production-latest.sh ./
-/usr/bin/bash ec2-prod-latest.sh
+aws s3 cp s3://ghg.rocks.image-gallery/ec2-prod-latest.sh ./ /usr/bin/bash ec2-prod-latest.sh
